27cd6066a3586952302d8953a7eb600eee9a6bce
Matthias Hecht <matthias@hecht.space>
2021-01-31 17:40:36 +0100

    release emapc version v1.3.0

--- content/projects/emapc/downloads/emapc-installer-linuxx64.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:75e3634e52763495a40cb8e4d6fb47da6aa8e2e788e9357a76d8de3fb418344c
-size 2048
---
+++ content/projects/emapc/downloads/v1-3-0-install-linuxx64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fff67d93664cb7d8719fcc3234f36b5aa5779ae87ca849124422019fbb0e4f61
+size 2048
